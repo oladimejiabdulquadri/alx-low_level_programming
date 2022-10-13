1bb82a416b9a77c441599ff86c18fb55bbdbe348
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-13 09:16:45 -0700

    alx haaa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,21 +3,16 @@
 
 
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return (0);
+		return (0);
 
 }
