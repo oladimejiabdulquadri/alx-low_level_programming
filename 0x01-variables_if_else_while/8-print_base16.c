08a77eac44676eb163de2463bd9b6b0ccd4daca2
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-16 11:29:07 -0700

    kij

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,12 +9,14 @@ int main(void)
 {
 	int i;
 
-	for (i = '0'; i <= '9'; i++);
+	for
+		(i = '0'; i <= '9'; i++);
 	{
 		putchar(i);
 
 	}
-	for (i = 'a'; i <= 'f'; i++);
+	for
+		(i = 'a'; i <= 'f'; i++);
 
 	{
 		putchar(i);
