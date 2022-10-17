6d45838dc708b34045f71c0539229eef93e6bd40
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-17 02:23:40 -0700

    momuyt

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,18 +10,12 @@ int main(void)
 {
 	int n;
 
-	for
-	{
-		(n = '0');
-		(n <= '9');
-		(n++);
-	}
+	for (n = '0'; n <= '9'; n++);
 
 	{
 		putchar(n);
 
-		if
-			(n != '9');
+		if (n != '9');
 
 		{
 			putchar(',');
