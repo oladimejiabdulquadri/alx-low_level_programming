84655a3b4af73a6bdeffb94aeec7f4fa21e9cf76
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-19 08:44:42 -0700

    taaaskool

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,18 @@
 #include "main.h"
-
 /**
- *Main - write a program to print _putchar
- * Return : return value should be 0 (success)
- *
- * Description : print _putchar and return 0
+*main - print _putchar
+*Description - use _putcahr only
+*Return: should always be 0
  */
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-	return (0);
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+	_putchar("\n");
 }
