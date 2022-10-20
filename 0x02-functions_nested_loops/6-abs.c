19f75ef8220da5bd6a17e770d35293f75ed3efa7
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 03:50:10 -0700

    jhgfdfdc

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *_abs- write the absolute value of an integer
+  *@n: the int in question
+  *Return: 0 or -0
+  */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (-0);
+	}
+}
