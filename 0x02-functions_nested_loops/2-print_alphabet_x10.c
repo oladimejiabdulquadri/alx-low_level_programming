fd3d06676f3eafc7633d92a6696ab5804791d1cf
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 03:07:46 -0700

    lkjhgfdrftyui

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@ void print_alphabet_x10(void)
 	char letter;
 
 	while
-		(count++ <= 9);
+		(count++ <= 9)
 	{
 	}
 
 	for
 		(letter = 'a';
 		 letter <= 'z';
-		 letter++);
+		 letter++)
 	{
 		_putchar(letter);
 
