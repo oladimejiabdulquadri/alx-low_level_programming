529ef5965e024014ebd073f5c3634b3f9a9f4689
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 14:32:43 -0700

    dscdc

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "mai.h"
+/**
+  *print_diagonal - print diagonal
+  *@n: is the number of times the char \ will be printed
+  * Return: always 0
+  */
+void print_diagonal(int n)
+{
+	int len, space;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+		{
+			for (space = 0; space < len; space++)
+				_putchar(' ');
+			_putchar('\\');
+
+			if (len == n - 1)
+				continue;
+
+			_putchar('\n');
+		}
+	}
+
+	_putchar('\n');
+}
