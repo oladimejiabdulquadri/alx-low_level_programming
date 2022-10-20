110050c33dbb558906f63ff6f6c23ea2492958e2
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 10:14:55 -0700

    uper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_isupper - find upper case letters
+ *@c: the char in question
+ * Return: return 1, otherwise 0
+ */
+int _isupper(int c);
+{
+	if
+		(c >= 'A';
+		 c <= 'Z';
+		 c++)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
