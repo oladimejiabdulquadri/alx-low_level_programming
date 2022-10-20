d320d825ca2c3b3eef3a514a35b9f336e0e510f8
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 13:04:46 -0700

    kljhk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *mul - multiply two munbers
+  *@a: first number
+  *@b: second number
+  * Return: 0
+  */
+int mul(int a, int b)
+{
+	int a;
+	int b;
+
+	int c = (a * b);
+
+	_putchar(c);
+	return (0);
+}
