f06b4b268cb97a91cae04ea111f4417034f000c2
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 02:53:22 -0700

    EDFG

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
   *_islower - check if c is lower case
-  *@c: is lower case
+  *@c: is the char to be checked
   *Return: 1 if c is lower case
   */
 int _islower(int c)
 {
 	if
-		(c >= 'a'; &&c <= 'z');
+		(c >= 'a'
+			&& c <= 'z');
 	{
 		return (1);
 	}
