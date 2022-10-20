4623ccab347fd7054a84f86c4f538632615f03ac
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 03:18:11 -0700

    lkjhgfdsertyuio

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+  *_isalpha - check for char alphabet
+  *Return: 1 if lower or upper, 0 otherwise
+  *@c: the char to be checked
+  */
+int _isalpha(int c)
+{
+	if
+		((c >= 'a' && c <= 'z') || (c >= 'a' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
