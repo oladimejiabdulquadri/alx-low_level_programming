a2aeb31761eeffe47ba3e84911a11ab146157435
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-20 02:06:05 -0700

    mummy

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_alphabet - print alphabets in  lower case
+*Description- use _putchar only twice
+*Return: should be..
+*/
+void print_alphabet(void)
+{
+	char letter;
+
+	for
+		(letter = 'a';
+		 letter <= 'z';
+		 letter++);
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
