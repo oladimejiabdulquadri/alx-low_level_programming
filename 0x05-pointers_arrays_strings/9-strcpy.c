a175af2cef272a1d1dc56e1b050434e431f26bd6
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-24 21:58:34 -0700

    die minute

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#inlude "main.h"
+/**
+  *_strcpy - prints string in pointed src to dest
+  *@src: first pointer
+  *@dest: second pointer
+  * Return: the string to dest
+  */
+void *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+
+	while (src[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
