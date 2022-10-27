eead8afb41b1a1c2da5521bc539b138473dfa428
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-27 15:55:27 -0700

    kkgkfjdfjjkkf

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -11,4 +11,6 @@ char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif /* MAIN_H */
