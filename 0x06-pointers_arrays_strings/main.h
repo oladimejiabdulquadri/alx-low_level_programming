4cb21b79dbcc9893c75f6b6c50e07fa2a9b2f2b8
oladimejiabdulquadri <husseinabdulafeez123@gmail.com>
2022-10-27 15:19:06 -0700

    lllllllll

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,5 @@ int _strcmp(char *s1, char *s2);
 char *_strncpy(char *dest, char *src, int n);
 char *_strncat(char *dest, char *src, int n);
 char *_strcat(char *dest, char *src);
-
+char *rot13(char *);
 #endif /* MAIN_H */
